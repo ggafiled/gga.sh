94b3e4c9db2d99439cd2ca5d0222d5318ae3ec36
ggafiled <gafewkik234@gmail.com>
2020-09-05 22:04:28 +0700

    [Fix bug] set cors options.

--- src/middlewares.js
+++ src/middlewares.js
@@ -21,7 +21,7 @@ function errorHandler(err, req, res, next) {
 
 function setHeader(req, res, next) {
 
-    res.setHeader('Access-Control-Allow-Origin', 'http://gga-sh.xyz');
+    res.setHeader('Access-Control-Allow-Origin', 'http://www.gga-sh.xyz');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
